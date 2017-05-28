5d79ba45decac2acf37e60c35d7c1a59327730e4
Dmitry Shechtman <damnian@gmail.com>
2017-05-28 18:01:22 +0300

    Add solution

---
+++ src/Net.Chdk.Providers.CameraModel/CameraModelProvider.cs
@@ -0,0 +1,24 @@
+﻿using Net.Chdk.Model.Camera;
+using Net.Chdk.Model.CameraModel;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Net.Chdk.Providers.CameraModel
+{
+    sealed class CameraModelProvider : ICameraModelProvider
+    {
+        IEnumerable<IProductCameraModelProvider> CameraModelProviders { get; }
+
+        public CameraModelProvider(IEnumerable<IProductCameraModelProvider> cameraModelProviders)
+        {
+            CameraModelProviders = cameraModelProviders;
+        }
+
+        public CameraModelInfo[] GetCameraModels(CameraInfo cameraInfo)
+        {
+            return CameraModelProviders
+                .Select(p => p.GetCameraModels(cameraInfo))
+                .FirstOrDefault(c => c != null);
+        }
+    }
+}
